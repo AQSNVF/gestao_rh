53d31b6b4ed0c73963b5e5a15eef9aaeab30ed20
Nelson Viana Freire <nelson Viana Freire>
2019-05-21 15:45:17 -0300

    Criando os relacionamentos de Banco de Dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from apps.colaboradores.models import Colaborador
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    proprietario = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.descricao
+
+
 
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from apps.colaboradores.models import Colaborador
 
 
 class Registrohoraextra(models.Model):
     motivo = models.CharField(max_length=100)
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.motivo
