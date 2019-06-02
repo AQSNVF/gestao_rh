5dd7cc9d074446976dedb01e1863f60c8d8ebd60
Nelson Viana Freire <nelson Viana Freire>
2019-06-02 13:04:55 -0300

    Configurando o Celery na Aplicação

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'apps.registro_hora_extra',
     'apps.core',
     'bootstrapform',
+    'django-celery-results',
 ]
 
 
@@ -118,7 +119,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-LOGIN_REDIRECT_URL = 'home',
+LOGIN_REDIRECT_URL = 'home'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_BROQKER_URL = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+
+
 
 
 
